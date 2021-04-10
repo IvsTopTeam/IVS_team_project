2acbfef6cc5ae0750490cc5dd8f3df64e9fdaf64
Tademait <Tademait@github.com>
2021-04-10 03:45:20 +0200

    batch of test containing div, sqrt and fact

--- LibraryTests.py
+++ LibraryTests.py
@@ -2,9 +2,10 @@ import unittest
 
 from TedovaKnihovna import *
 
-# TODO  add tests with flots to our_add
 # TODO  add tests for other methods
-# TODO  find out how to import the entire library properly
+# TODO  find out how to import the entire library properly + change the lib name
+# TODO  find out where using AlmostEqual instead of Equal is a good idea
+
 
 class TestAdd(unittest.TestCase):
     def test_basic(self):
@@ -45,7 +46,7 @@ class TestAdd(unittest.TestCase):
         self.assertEqual(-0.025, result)
 
 
-class testSub(unittest.TestCase):
+class TestSub(unittest.TestCase):
     def test_small_nums(self):
         result = our_sub(10, 30)
         self.assertEqual(40, result)
@@ -70,7 +71,8 @@ class testSub(unittest.TestCase):
         result = our_sub(50_000.7, 30_000.5)
         self.assertEqual(20_000.2, result)
 
-class testMul(unittest.TestCase):
+
+class TestMul(unittest.TestCase):
     def test_small_nums(self):
         result = our_mul(10, 30)
         self.assertEqual(300, result)
@@ -93,7 +95,103 @@ class testMul(unittest.TestCase):
 
     def test_float_large(self):
         result = our_mul(50_000.7, 30_000.5)
-        self.assertEqual(1_500_046_000,35, result)
+        self.assertEqual(1_500_046_000.35, result)
+
+
+class TestDiv(unittest.TestCase):
+    def test_small_nums(self):
+        result = our_div(50, 10)
+        self.assertEqual(5, result)
+
+    def test_big_nums(self):
+        result = our_div(10_000_000, 50_000)
+        self.assertEqual(200, result)
+
+    def test_small_negative(self):
+        result = our_div(-340, -20)
+        self.assertEqual(17, result)
+
+    def test_large_negative(self):
+        result = our_div(-20_000_000, 1000)
+        self.assertEqual(-20_000, result)
+
+    def test_float_small(self):
+        result = our_div(0.002, 0.001)
+        self.assertEqual(2, result)
+
+    def test_float_large(self):
+        result = our_div(50_000.7, 30_000.5)
+        self.assertAlmostEqual(1.6666622222962950617489708504858, result, 20)
+
+
+class TestFact(unittest.TestCase):
+    def test_small_num(self):
+        result = our_fact(4)
+        self.assertEqual(24, result)
+
+    def test_zero(self):
+        result = our_fact(0)
+        self.assertEqual(1, result)
+
+    def test_float(self):
+        with self.assertRaises(Exception):
+            our_fact(2.5)
+
+    def test_negative(self):
+        with self.assertRaises(Exception):
+            our_fact(-10)
+
+
+class TestPow(unittest.TestCase):
+    def test_small_nums(self):
+        result = our_pow(10, 3)
+        self.assertEqual(1000, result)
+
+    def test_large_nums(self):
+        result = our_pow(10, 10)
+        self.assertEqual(10_000_000_000, result)
+
+    def test_small_negative(self):
+        result = our_pow(-20, -2)
+        self.assertAlmostEqual(0.0025, result, 20)
+
+    def test_large_negative(self):
+        result = our_pow(-100, 10)
+        self.assertEqual(100_000_000_000_000_000_000, result)
+
+    def test_float_small(self):
+        result = our_pow(0.002, 0.001)
+        self.assertAlmostEqual(0.99380466263779646744326432913976, result, 20)
+
+    def test_float_large(self):
+        result = our_pow(10.5, 3.4)
+        self.assertAlmostEqual(2_965.129166883078114339313070216, result, 20)
+
+
+class TestSqrt(unittest.TestCase):
+    def test_small_nums(self):
+        result = our_sqrt(2, 100)
+        self.assertEqual(10, result)
+
+    def test_large_nums(self):
+        result = our_sqrt(3, 100_000_000)
+        self.assertEqual(30_000, result)
+
+    def test_small_negative(self):
+        result = our_sqrt(-20, -2)
+        self.assertAlmostEqual(0.0025, result, 20)
+
+    def test_large_negative(self):
+        result = our_sqrt(-100, 10)
+        self.assertEqual(100_000_000_000_000_000_000, result)
+
+    def test_float_small(self):
+        result = our_sqrt(0.002, 0.001)
+        self.assertAlmostEqual(0.99380466263779646744326432913976, result, 20)
+
+    def test_float_large(self):
+        result = our_sqrt(10.5, 3.4)
+        self.assertAlmostEqual(2_965.129166883078114339313070216, result, 20)
 
 
 if __name__ == '__main__':
