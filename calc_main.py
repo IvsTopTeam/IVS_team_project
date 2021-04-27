7f82c1c52333fd7ec7945d2346a16771c73590c6
xbubak01 <xbubak01@vutbr.cz>
2021-04-28 00:07:57 +0200

    documentation

--- calc_main.py
+++ calc_main.py
@@ -1,5 +1,26 @@
 import calc_gui
 
+
+##
+# @mainpage Program Documentation
+# @tableofcontents
+# @subsection TopTeam Calculator
+# A simple implementation of arithmetic calculator for second IVS project with main purpose - focus on TEAM WORK.
+#
+# @subsection Documentation
+# In this documentation you can find all the elements that our project is composed of.
+#
+# For quick access click on <a href="file:///home/xbubak01/Documents/IVS/project2/IVS_team_project/doc/html/files.html">Files</a>,
+# choose one that you are interested in and learn more about all the functions, variables, etc. used.
+#
+# It includes mathematic library, main function structure with GUI essentials and tests having set the foundation of program development (TDD).
+#
+
+##
+# @file calc_main.py
+# @brief Essential file building the program
+#
+
 app = calc_gui.QApplication([])
 window = calc_gui.GUI()
 window.show()
