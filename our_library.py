a7f71e1a0c87451990f07898b3114d6f3eb6c79f
Elinka <alena.klimecka@gmail.com>
2021-04-27 20:32:21 +0200

    fixed error with NaN

--- our_library.py
+++ our_library.py
@@ -15,13 +15,15 @@ def our_mul(num1: float, num2: float) -> float:
 
 # funkce dostane dva parametry a vraci jejich podil
 def our_div(num1: float, num2: float) -> float:
+    if num2 == 0:           # nelze delit nulou
+        return float('NaN')
     return num1 / num2
 
 
 # funkce dostane jeden parametr a vraci jeho faktorial
-def our_fact(num: int) -> float:    # faktorial lze zavolat pouze s celym cislem
+def our_fact(num: int) -> float:    # faktorial lze zavolat pouze s celym kladnym cislem
     if num < 0:
-        raise Exception
+        return float('NaN')
     factorial: int = 1
     for i in range(1, num + 1):
         factorial = factorial * i
@@ -30,13 +32,17 @@ def our_fact(num: int) -> float:    # faktorial lze zavolat pouze s celym cislem
 
 # funkce dostane dva parametry, vraci mocninu num^(exp)
 def our_pow(num: float, exp: float) -> float:
+    if (num == 0) & (exp < 0):
+        return float('NaN')
     return num ** exp
 
 
 # funkce dostane dva parametry, vraci jejich num1√(num2)
 def our_sqrt(num1: float, num2: float) -> float:     # nelze zavolat s num1 == 0
     if num1 == 0:
-        raise Exception
+        return float('NaN')
+    if (num1 < 0) & (num2 == 0):
+        return float('NaN')
     root = our_pow(num2, 1/num1)
     return root
 
