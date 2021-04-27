b89e89badc1bfc89426e42251fb3bb800fe7eb8e
Elinka <alena.klimecka@gmail.com>
2021-04-27 12:29:34 +0200

    finished standard deviation

--- stddev.py
+++ stddev.py
@@ -1,24 +1,27 @@
 from our_library import *
+import cProfile
 
 
-def main():
-    num_list = list(map(int, input().split()))
+def dev():
+    num_list = list(map(int, input().split()))      # nacte cisla do listu
+
+    count = len(num_list)                           # N
+
     sum_std = 0
-    count = len(num_list)
-
     for i in range(count):
-        sum_std = our_add(sum_std, num_list[i])
+        sum_std = our_add(sum_std, num_list[i])     # suma vsech cisel v listu
 
-    average = our_div(sum_std, count)
+    average = our_div(sum_std, count)               # prumer cisel
 
-    sum_tot = 0
-    help_num = our_mul(count, our_pow(average, 2))
+    sum_pow = 0
     for i in range(count):
-        sum_tot = our_add(sum_tot, our_sub(our_pow(num_list[i], 2), help_num))
+        sum_pow = our_add(sum_pow, our_pow(num_list[i], 2))     # suma vsech (cisel na druhou)
 
-    total = our_sqrt(2, our_div(sum_tot, our_sub(count, 1)))
-    print(total)
+    # vypocet vzorcem pro vyberovou smerodatnou odchylku
+    total_help = our_mul(count, our_pow(average, 2))            # N*(avg^2)
+    total = our_sqrt(2, our_div(our_sub(sum_pow, total_help), our_sub(count, 1)))
+
+    print(round(total, 3))
 
 
-if __name__ == "__main__":
-    main()
+cProfile.run('dev()')
