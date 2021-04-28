c0f27c4d6fe9c60915bcaec2b99059bc120305e1
Elinka <alena.klimecka@gmail.com>
2021-04-28 18:03:28 +0200

    changed comments

--- stddev.py
+++ stddev.py
@@ -2,21 +2,21 @@ from our_library import *
 
 
 def dev():
-    num_list = list(map(int, input().split()))      # nacte cisla do listu
+    num_list = list(map(int, input().split()))      # read the numbers
 
     count = len(num_list)                           # N
 
     sum_std = 0
     for i in range(count):
-        sum_std = our_add(sum_std, num_list[i])     # suma vsech cisel v listu
+        sum_std = our_add(sum_std, num_list[i])     # the sum of all numbers
 
-    average = our_div(sum_std, count)               # prumer cisel
+    average = our_div(sum_std, count)               # average of numbers
 
     sum_pow = 0
     for i in range(count):
-        sum_pow = our_add(sum_pow, our_pow(num_list[i], 2))     # suma vsech (cisel na druhou)
+        sum_pow = our_add(sum_pow, our_pow(num_list[i], 2))     # sum of all (number^2)
 
-    # vypocet vzorcem pro vyberovou smerodatnou odchylku
+    # calculated by the formula for the sample standard deviation
     total_help = our_mul(count, our_pow(average, 2))            # N*(avg^2)
     total = our_sqrt(2, our_div(our_sub(sum_pow, total_help), our_sub(count, 1)))
 
